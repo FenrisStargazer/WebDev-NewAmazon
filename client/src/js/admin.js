0476c2860d59ef633a70db95c6ae9418773aab5e
dpeterson8 <dpeterson8@ewu.edu>
2023-03-07 19:51:23 -0800

    a lot

--- client/src/js/admin.js
+++ client/src/js/admin.js
@@ -1,3 +1,75 @@
+window.onload = function() {
+    let accordianItems = document.getElementsByClassName('accordion-collapse');
+
+    accordianItems.forEach(element => {
+        
+        const  deleteButton = document.createElement('button');
+        const deleteI = document.createElement('i');
+
+        deleteButton.setAttribute('type', 'button');
+        deleteButton.setAttribute('class', 'btn btn-danger');
+
+        deleteI.setAttribute('class', 'bi bi-trash')
+
+        deleteButton.appendChild(deleteI);
+
+        element.appendChild(deleteButton);
+
+        const editButton = document.createElement('button');
+        const editI = document.createElement('i');
+
+        deleteButton.setAttribute('type', 'button');
+        deleteButton.setAttribute('class', 'btn btn-warning');
+
+        deleteI.setAttribute('class', 'bi bi-tools')
+
+    });
+
+};
+
+function addNewWork() {
+    let label = document.getElementById('new-label');
+    let content = document.getElementById('new-content');
+
+    fetch(`http://localhost:3000/work/${label.value}/${content.value}`, {
+        headers: {
+            'Accept': 'application/json'
+        },
+        method: "POST"
+    })
+
+    .then(response => {
+        return response.status;
+    })
+
+    .then(status => {
+        alert(status);
+    })
+
+
+}
+
+function deleteWork(id) {
+    // let id = document.getElementsByClassName('accordion-item').id.value;
+
+    fetch(`http://localhost:3000/work/${id}`, {
+        headers: {
+            'Accept': 'application/json'
+        },
+        method: "DELETE"
+    })
+
+    .then(response => {
+        return response.status;
+    })
+
+    .then(status => {
+        alert(status);
+    })
+
+
+}
+
 function logAdmin() {
     let username = document.getElementById('admin-name').value;
     let password = document.getElementById('admin-pass').value;
@@ -14,9 +86,9 @@ function logAdmin() {
     .then(response => {
         if(response.status == 201) {
             // Sign user in
-    
-            alert(`Loged in!`);
-            document.location.href = 'index.html';
+            const temp = "hello";
+            alert(`${temp}`);
+            document.location.href = 'admin.html';
     
         } else {
             // Alert user of bad credentials
@@ -25,4 +97,8 @@ function logAdmin() {
     
         }
     })
+}
+
+function hey() {
+    alert('hey');
 }
